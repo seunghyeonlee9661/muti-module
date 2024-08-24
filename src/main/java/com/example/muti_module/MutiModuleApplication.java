5ced9e5a52210a6ab170c3c2870c01d423eb9a38
SeungHyun Lee <101535408+seunghyeonlee9661@users.noreply.github.com>
2024-08-24 14:51:08 +0900

    Initial commit

---
+++ src/main/java/com/example/muti_module/MutiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.muti_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutiModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/muti_module/MutiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muti_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
